9a21e9519d1eca520980e13380b98efd5894044d
forzq <shablinskiy2005@mail.ru>
2024-12-04 01:30:01 +0300

    feat: middleware end

--- server/routes/UsersRouter.js
+++ server/routes/UsersRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const UsersController = require('../controllers/UsersController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', UsersController.registration)
 router.post('/login', UsersController.login)
-router.get('/auth', UsersController.check)
+router.get('/auth', authMiddleware, UsersController.check)
 
 module.exports = router
